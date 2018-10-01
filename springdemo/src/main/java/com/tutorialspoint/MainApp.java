156ed423fa08bbd1d45f5fecf20c0e4761255439
jdl <jdl@JDL-DESKTOP>
2018-10-01 16:12:23 +0800

    注入集合

--- springdemo/src/main/java/com/tutorialspoint/MainApp.java
+++ springdemo/src/main/java/com/tutorialspoint/MainApp.java
@@ -5,15 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	
 	public static void main(String[] args) {
-		System.out.println("he");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+		jc.getAddressList();
+		jc.getAddressSet();
+		jc.getAddressMap();
+		jc.getAddressProp();
+		context.close();
 	}
-//	public static void main(String[] args) {
-//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-//		JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
-//		jc.getAddressList();
-//		jc.getAddressSet();
-//		jc.getAddressMap();
-//		jc.getAddressProp();
-//		context.close();
-//	}
 }
