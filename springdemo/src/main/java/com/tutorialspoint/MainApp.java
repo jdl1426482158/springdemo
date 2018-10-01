24b6e8d3eae782a25af0aa69e501bf7473e6c2fc
jdl <jdl@JDL-DESKTOP>
2018-10-01 15:53:40 +0800

    找不到主类

--- springdemo/src/main/java/com/tutorialspoint/MainApp.java
+++ springdemo/src/main/java/com/tutorialspoint/MainApp.java
@@ -3,10 +3,17 @@ package com.tutorialspoint;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
+	
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		TextEditor te = (TextEditor) context.getBean("textEditor");
-		te.spellCheck();
-		context.close();
+		System.out.println("he");
 	}
+//	public static void main(String[] args) {
+//		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+//		JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+//		jc.getAddressList();
+//		jc.getAddressSet();
+//		jc.getAddressMap();
+//		jc.getAddressProp();
+//		context.close();
+//	}
 }
